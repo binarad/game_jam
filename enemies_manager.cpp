5d1c3c6be8e1a411c7cbf16571bc77a702c6cbc9
binarad <pozhidaev.ruslan@gmail.com>
2025-08-17 18:45:50 +0300

    Added enemy class and random texture for each enemy

--- enemies_manager.cpp
+++ enemies_manager.cpp
@@ -47,7 +47,7 @@ void EnemiesManager::update(Flower &flower, int &game_phase, float frame_time)
 
     // phase change
     timer_update(m_phase_change_timer, frame_time);
-    if (timer_is_finished(m_phase_change_timer)) 
+    if (timer_is_finished(m_phase_change_timer))
     {
         timer_restart(m_phase_change_timer);
 
@@ -57,7 +57,7 @@ void EnemiesManager::update(Flower &flower, int &game_phase, float frame_time)
         m_move_speed *= ENEMIES_PHASE_MOVE_SPEED_INCREASE_MUL;
     }
 
-    return;
+    // return;
 
     // spawn
     // std::cout << m_spawn_timeout << std::endl;
@@ -67,13 +67,17 @@ void EnemiesManager::update(Flower &flower, int &game_phase, float frame_time)
         // we change spawn timeout on phase change
         m_spawn_timer = timer_start(m_spawn_timeout);
 
+        int texture_index = rand_i(0, static_cast<int>(m_enemy_textures.size() - 1));
+        SpriteSheet &chosen_texture = m_enemy_textures[texture_index];
+
         auto pos = rand_coord_in_range(wr_vec2({50, 50}), ENEMIES_SPAWN_MIN_RANGE, ENEMIES_SPAWN_MAX_RANGE);
-        m_list.push_back({pos.x, pos.y, ENEMY_SIZE.x, ENEMY_SIZE.y});
+        // m_list.push_back({pos.x, pos.y, ENEMY_SIZE.x, ENEMY_SIZE.y});
+        m_list.emplace_back(pos.x, pos.y, ENEMY_SIZE.x, ENEMY_SIZE.y, chosen_texture);
     }
 
-    for (auto &enemy_rect : m_list)
+    for (Enemy &enemy : m_list)
     {
-        rect_move_towards_pos(enemy_rect, wr_vec2({50, 50}), m_move_speed, frame_time);
+        rect_move_towards_pos(enemy.bounds, wr_vec2({50, 50}), m_move_speed, frame_time);
     }
 
     _damage_flower(flower, frame_time);
@@ -92,14 +96,15 @@ void EnemiesManager::draw_phase(int game_phase)
 
 void EnemiesManager::draw(SpriteSheet &enemy_sprite)
 {
-    for (auto enemy_rect : m_list)
+
+    for (Enemy enemy : m_list)
     {
         // TODO: what is x, y 50 ?
-        if (enemy_rect.x > wr_x(50))
-            enemy_sprite.draw_flipped(0, enemy_rect, WHITE, true, false);
+        if (enemy.bounds.x > wr_x(50))
+            enemy.texture.draw_flipped(0, enemy.bounds, WHITE, true, false);
 
-        if (enemy_rect.x < wr_x(50))
-            enemy_sprite.draw(0, enemy_rect, WHITE);
+        if (enemy.bounds.x < wr_x(50))
+            enemy.texture.draw(0, enemy.bounds, WHITE);
         // DrawRectangleRec(enemy_rect, COLOR_YELLOW);
     }
 }
@@ -108,7 +113,7 @@ void EnemiesManager::remove_clicked_enemies(Vector2 mouse_pos)
 {
     for (size_t index = 0; index < m_list.size(); index++)
     {
-        if (CheckCollisionCircleRec(mouse_pos, EXPLOSION_RADIUS, m_list[index]))
+        if (CheckCollisionCircleRec(mouse_pos, EXPLOSION_RADIUS, m_list[index].bounds))
         {
             m_list.erase(m_list.begin() + index);
         }
@@ -121,13 +126,14 @@ void EnemiesManager::_damage_flower(Flower &flower, float frame_timer)
     timer_update(m_damage_timer, frame_timer);
 
     // cant attack until timer finished
-    if (timer_is_in_progress(m_damage_timer)) {
+    if (timer_is_in_progress(m_damage_timer))
+    {
         return;
     }
 
-    for (auto enemy : m_list)
+    for (Enemy enemy : m_list)
     {
-        if (CheckCollisionRecs(enemy, flower.flower_bounds))
+        if (CheckCollisionRecs(enemy.bounds, flower.flower_bounds))
         {
             timer_restart(m_damage_timer);
             flower.set_hp(flower.get_hp() - 10.0f);
@@ -136,4 +142,21 @@ void EnemiesManager::_damage_flower(Flower &flower, float frame_timer)
             break;
         }
     }
+}
+
+void EnemiesManager::load_enemy_textures()
+{
+    for (int index = 1; index < 10; index++)
+    {
+        std::string texture_path = "assets/enemy-" + std::to_string(index) + ".png";
+        SpriteSheet texture;
+        texture.load(texture_path.c_str(), 16);
+        m_enemy_textures[index - 1] = texture;
+    }
+}
+
+void EnemiesManager::unload_enemy_textures()
+{
+    for (SpriteSheet texture : m_enemy_textures)
+        texture.unload();
 }
--- enemies_manager.h
+++ enemies_manager.h
@@ -2,9 +2,11 @@
 
 #include <raylib.h>
 #include <vector>
+#include <array>
 #include "rutils.h"
 #include "sprite_sheet.h"
 #include "flower.h"
+#include "enemy.h"
 
 const float EXPLOSION_RADIUS = wr_y(5);
 
@@ -17,11 +19,12 @@ public:
     void update(Flower &flower, int &game_phase, float frame_time);
     void draw(SpriteSheet &enemy_sprite);
     void draw_phase(int game_phase);
-    // TODO: rename
     void remove_clicked_enemies(Vector2 mouse_pos);
+    void load_enemy_textures();
+    void unload_enemy_textures();
 
 private:
-    std::vector<Rectangle> m_list;
+    std::vector<Enemy> m_list;
 
     float m_move_speed;
     float m_spawn_timeout;
@@ -29,6 +32,8 @@ private:
     Timer m_spawn_timer;
     Timer m_damage_timer;
     Timer m_phase_change_timer;
+
+    std::array<SpriteSheet, 9> m_enemy_textures;
 
     void _damage_flower(Flower &flower, float frame_time);
 };
